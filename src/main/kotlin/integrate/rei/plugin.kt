4d9d157b5264c26b3834828d80bd7e5aa7b5cb3d
xtexChooser <zbwxtex@qq.com>
2022-02-25 22:47:47 +0800

    feat(integrate): add support for REI

---
+++ src/main/kotlin/integrate/rei/plugin.kt
@@ -0,0 +1,37 @@
+package com.xtex.scpsharp.integrate.rei
+
+import com.xtex.scpsharp.content.scp914.SCP914ControllerBlock
+import com.xtex.scpsharp.content.scp914.SCP914Recipe
+import com.xtex.scpsharp.integrate.rei.SCP914REICategory
+import com.xtex.scpsharp.integrate.rei.SCP914REIDisplay
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry
+import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay
+import me.shedaniel.rei.api.common.entry.type.EntryTypeRegistry
+import me.shedaniel.rei.api.common.plugins.REIServerPlugin
+import me.shedaniel.rei.api.common.util.EntryIngredients
+
+object REIServerPlugin : REIServerPlugin {
+
+    override fun registerDisplaySerializer(registry: DisplaySerializerRegistry) {
+        registry.register(SCP914REICategory.identifier, BasicDisplay.Serializer.ofSimple(::SCP914REIDisplay))
+    }
+
+}
+
+object REIClientPlugin : REIClientPlugin {
+
+    override fun registerCategories(registry: CategoryRegistry) {
+        registry.add(SCP914REICategory)
+        registry.removePlusButton(SCP914REICategory.identifier)
+        registry.addWorkstations(SCP914REICategory.identifier, EntryIngredients.of(SCP914ControllerBlock.item))
+    }
+
+    override fun registerDisplays(registry: DisplayRegistry) {
+        registry.registerRecipeFiller(SCP914Recipe::class.java, SCP914Recipe.type, ::SCP914REIDisplay)
+    }
+
+}
---
+++ src/main/kotlin/integrate/rei/scp914.kt
@@ -0,0 +1,60 @@
+package com.xtex.scpsharp.integrate.rei
+
+import com.xtex.scpsharp.content.scp914.SCP914ControllerBlock
+import com.xtex.scpsharp.content.scp914.SCP914Recipe
+import com.xtex.scpsharp.util.id
+import me.shedaniel.math.Point
+import me.shedaniel.math.Rectangle
+import me.shedaniel.rei.api.client.gui.Renderer
+import me.shedaniel.rei.api.client.gui.widgets.Widget
+import me.shedaniel.rei.api.client.gui.widgets.Widgets
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory
+import me.shedaniel.rei.api.common.category.CategoryIdentifier
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay
+import me.shedaniel.rei.api.common.entry.EntryIngredient
+import me.shedaniel.rei.api.common.util.EntryIngredients
+import me.shedaniel.rei.api.common.util.EntryStacks
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Identifier
+import java.util.*
+
+object SCP914REICategory : DisplayCategory<SCP914REIDisplay> {
+
+    val identifier: CategoryIdentifier<SCP914REIDisplay> = CategoryIdentifier.of(id("scp-914"))
+
+    override fun getIcon(): Renderer = EntryStacks.of(SCP914ControllerBlock.item)
+
+    override fun getTitle() = TranslatableText("category.rei.scp914")
+
+    override fun getCategoryIdentifier() = identifier
+
+    override fun setupDisplay(display: SCP914REIDisplay, bounds: Rectangle): List<Widget> {
+        return listOf<Widget>(
+            Widgets.createRecipeBase(bounds),
+            Widgets.createSlot(Point(bounds.centerX - 26 - 9, bounds.centerY - 9))
+                .markInput()
+                .entries(display.inputEntries[0]),
+            Widgets.createArrow(Point(bounds.centerX - 9, bounds.centerY - 9)),
+            Widgets.createResultSlotBackground(Point(bounds.centerX + 26, bounds.centerY - 9)),
+            Widgets.createSlot(Point(bounds.centerX + 26, bounds.centerY - 9))
+                .markOutput()
+                .disableBackground()
+                .entries(display.outputEntries[0]),
+        )
+    }
+
+    override fun getDisplayHeight() = 42
+
+}
+
+class SCP914REIDisplay(inputs: List<EntryIngredient>, outputs: List<EntryIngredient>, location: Optional<Identifier>) :
+    BasicDisplay(inputs, outputs, location) {
+
+    constructor(recipe: SCP914Recipe) : this(
+        listOf(EntryIngredients.ofIngredient(recipe.source)), listOf(EntryIngredients.of(recipe.output)),
+        Optional.of(recipe.id)
+    )
+
+    override fun getCategoryIdentifier() = SCP914REICategory.identifier
+
+}
