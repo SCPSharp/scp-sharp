cbf5790d0f09381cdde41e711c86d912086ee71b
xtexChooser <xtexchooser@duck.com>
2023-01-11 10:08:26 +0800

    style: fix license header

--- src/main/kotlin/scpsharp/content/subject/scp500/SCP500.kt
+++ src/main/kotlin/scpsharp/content/subject/scp500/SCP500.kt
@@ -2,7 +2,6 @@
  * Copyright (C) 2023  SCPSharp Team
  *
  * This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. See LICENSE file for more.
- * ./grad
  */
 package scpsharp.content.subject.scp500
 
