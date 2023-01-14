dfb08922badb599aaded8eed0276db52b28f15ef
xtexChooser <xtexchooser@duck.com>
2023-01-14 09:29:31 +0800

    chore!: remove tag minecraft:block/scpsharp:facility_keep

--- src/main/kotlin/scpsharp/content/facility/FacilityTags.kt
+++ src/main/kotlin/scpsharp/content/facility/FacilityTags.kt
@@ -5,15 +5,11 @@
  */
 package scpsharp.content.facility
 
-import net.minecraft.block.Block
-import net.minecraft.registry.Registries
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
 import scpsharp.util.id
 
 object FacilityTags {
-
-    val FACILITY_KEEP: TagKey<Block> = TagKey.of(Registries.BLOCK.key, id("facility_keep"))
 
     val START = TagKey.of(RegistryKeys.STRUCTURE_PIECE, id("facility/start"))!!
     val PART = TagKey.of(RegistryKeys.STRUCTURE_PIECE, id("facility/part"))!!
