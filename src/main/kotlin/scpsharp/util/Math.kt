992914ee3de12e282e25acb22ef3af4206c1d6cd
xtexChooser <xtexchooser@duck.com>
2023-01-14 10:33:14 +0800

    fix!: coerce

--- src/main/kotlin/scpsharp/util/Math.kt
+++ src/main/kotlin/scpsharp/util/Math.kt
@@ -5,16 +5,20 @@ import net.minecraft.util.math.BlockBox
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3i
+import kotlin.math.max
+import kotlin.math.min
 
 fun BlockBox(pos1: BlockPos, pos2: BlockPos): BlockBox =
     BlockBox.create(Vec3i(pos1.x, pos1.y, pos1.z), Vec3i(pos2.x, pos2.y, pos2.z))
 
-@Suppress("DEPRECATION")
-operator fun BlockBox.plus(box: BlockBox): BlockBox = encompass(box)
+operator fun BlockBox.plus(box: BlockBox) = BlockBox(
+    min(minX, box.minX), min(minY, box.minY), min(minZ, box.minZ),
+    max(maxX, box.maxX), max(maxY, box.maxY), max(maxZ, box.maxZ),
+)
 
 fun BlockBox.coerce(box: BlockBox) = BlockBox(
     minX.coerceAtLeast(box.minX), minY.coerceAtLeast(box.minY), minZ.coerceAtLeast(box.minZ),
-    minX.coerceAtMost(box.minX), minY.coerceAtMost(box.minY), minZ.coerceAtMost(box.minZ),
+    maxX.coerceAtMost(box.maxX), maxY.coerceAtMost(box.maxY), maxZ.coerceAtMost(box.maxZ),
 )
 
 val Direction.asBlockRotation
