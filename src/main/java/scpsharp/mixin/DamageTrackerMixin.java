2598b0558f79f49250d57b449bb31a82b26f1fd7
xtex <xtexchooser@duck.com>
2023-07-23 23:08:13 +0800

    fix: SCP-008 infection tracker

--- src/main/java/scpsharp/mixin/DamageTrackerMixin.java
+++ src/main/java/scpsharp/mixin/DamageTrackerMixin.java
@@ -6,15 +6,12 @@
 package scpsharp.mixin;
 
 import kotlin.collections.CollectionsKt;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTracker;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.property.Properties;
 import net.minecraft.world.chunk.WorldChunk;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,33 +25,30 @@ import java.util.Objects;
 @Mixin(DamageTracker.class)
 public abstract class DamageTrackerMixin {
 
-    @Shadow
-    @Final
-    public abstract LivingEntity getEntity();
-
     @Inject(
-            method = "onDamage(Lnet/minecraft/entity/damage/DamageSource;FF)V",
+            method = "onDamage(Lnet/minecraft/entity/damage/DamageSource;F)V",
             at = @At("RETURN")
     )
-    public void onDamage(DamageSource damageSource, float originalHealth, float damage, CallbackInfo info) {
+    public void onDamage(DamageSource damageSource, float damage, CallbackInfo info) {
         // Infect SCP-008 from containment block
-        var world = getEntity().getWorld();
+        var entity = ((DamageTracker) (Object) this).entity;
+        var world = entity.getWorld();
         var chunks = new WorldChunk[]{
-                world.getWorldChunk(getEntity().getBlockPos().add(6, 0, 6)),
-                world.getWorldChunk(getEntity().getBlockPos().add(6, 0, -6)),
-                world.getWorldChunk(getEntity().getBlockPos().add(-6, 0, 6)),
-                world.getWorldChunk(getEntity().getBlockPos().add(-6, 0, -6)),
+                world.getWorldChunk(entity.getBlockPos().add(6, 0, 6)),
+                world.getWorldChunk(entity.getBlockPos().add(6, 0, -6)),
+                world.getWorldChunk(entity.getBlockPos().add(-6, 0, 6)),
+                world.getWorldChunk(entity.getBlockPos().add(-6, 0, -6)),
         };
         var entities = CollectionsKt.flatMap(Arrays.asList(chunks), (chunk) -> chunk.getBlockEntities().values());
-        entities = CollectionsKt.filter(entities, (entity) -> entity instanceof SCP008ContainmentBlockEntity);
-        entities = CollectionsKt.filter(entities, (entity) -> entity.getPos().getSquaredDistance(getEntity().getBlockPos()) <= 6 * 6);
-        entities = CollectionsKt.filter(entities, (entity) -> Objects.requireNonNull(entity.getWorld()).getBlockState(entity.getPos()).get(Properties.OPEN));
-        for (var entity : entities) {
-            SCP008.INSTANCE.getLOGGER().info(getEntity() + " got " + damageSource + " x" + damage + " and getting infected from a open containment box at " + entity.getPos());
-            if (getEntity() != null && getEntity() instanceof PlayerEntity playerEntity) {
+        entities = CollectionsKt.filter(entities, (blockEntity) -> blockEntity instanceof SCP008ContainmentBlockEntity);
+        entities = CollectionsKt.filter(entities, (blockEntity) -> blockEntity.getPos().getSquaredDistance(entity.getBlockPos()) <= 6 * 6);
+        entities = CollectionsKt.filter(entities, (blockEntity) -> Objects.requireNonNull(blockEntity.getWorld()).getBlockState(blockEntity.getPos()).get(Properties.OPEN));
+        for (var blockEntity : entities) {
+            SCP008.INSTANCE.getLOGGER().info(entity + " got " + damageSource + " x" + damage + " and getting infected from a open containment box at " + blockEntity.getPos());
+            if (entity instanceof PlayerEntity playerEntity) {
                 playerEntity.incrementStat(SCP008.INSTANCE.getINFECTING_STAT());
             }
-            SCP008StatusEffect.INSTANCE.infect(getEntity(), damageSource.getSource());
+            SCP008StatusEffect.INSTANCE.infect(entity, damageSource.getSource());
             return;
         }
     }
